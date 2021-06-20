f0d18ec5089968db60e4c115c9b2019f84d06232
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-06-20 22:08:04 +1000

    Implement BIDS Exec (#9)

--- fnirsapp_qr.py
+++ fnirsapp_qr.py
@@ -15,10 +15,10 @@ from itertools import compress
 import os
 import subprocess
 
-__version__ = "v0.0.13"
+__version__ = "v0.1.0"
 
 
-def run(command, env={}):
+def fnirsapp_qr(command, env={}):
     merged_env = os.environ
     merged_env.update(env)
     process = subprocess.Popen(command, stdout=subprocess.PIPE,
@@ -35,27 +35,29 @@ def run(command, env={}):
 
 
 parser = argparse.ArgumentParser(description='Quality Reports')
-parser.add_argument('--bids_dir', default="/bids_dataset", type=str,
+parser.add_argument('--input-datasets', default="/bids_dataset", type=str,
                     help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
-parser.add_argument('--sci_threshold', type=float, default=0.0,
-                    help='Threshold below which a channel is marked as bad.')
-parser.add_argument('--pp_threshold', type=float, default=0.0,
-                    help='Threshold below which a channel is marked as bad.')
-parser.add_argument('--participant_label',
+parser.add_argument('--output-location', default="/bids_dataset/derivatives/fnirs-apps-quality-reports",
+                    type=str, help='The directory where the output files should be stored.')
+parser.add_argument('--subject-label',
                     help='The label(s) of the participant(s) that should be '
                     'analyzed. The label corresponds to '
-                    'sub-<participant_label> from the BIDS spec (so it does '
+                    'sub-<subject-label> from the BIDS spec (so it does '
                     'not include "sub-"). If this parameter is not provided '
                     'all subjects should be analyzed. Multiple participants '
                     'can be specified with a space separated list.',
                     nargs="+")
-parser.add_argument('--task_label',
+parser.add_argument('--task-label',
                     help='The label(s) of the tasks(s) that should be '
                     'analyzed. If this parameter is not provided '
                     'all tasks should be analyzed. Multiple tasks '
                     'can be specified with a space separated list.',
                     nargs="+")
+parser.add_argument('--sci-threshold', type=float, default=0.0,
+                    help='Threshold below which a channel is marked as bad.')
+parser.add_argument('--pp-threshold', type=float, default=0.0,
+                    help='Threshold below which a channel is marked as bad.')
 parser.add_argument('-v', '--version', action='version',
                     version='BIDS-App Scalp Coupling Index version '
                     f'{__version__}')
@@ -69,11 +71,11 @@ args = parser.parse_args()
 
 ids = []
 # only for a subset of subjects
-if args.participant_label:
-    ids = args.participant_label
+if args.subject_label:
+    ids = args.subject_label
 # for all subjects
 else:
-    subject_dirs = glob(op.join(args.bids_dir, "sub-*"))
+    subject_dirs = glob(op.join(args.input_datasets, "sub-*"))
     ids = [subject_dir.split("-")[-1] for
            subject_dir in subject_dirs]
     print(f"No participants specified, processing {ids}")
@@ -83,7 +85,7 @@ tasks = []
 if args.task_label:
     tasks = args.task_label
 else:
-    all_snirfs = glob(f"{args.bids_dir}/**/*_nirs.snirf", recursive=True)
+    all_snirfs = glob(f"{args.input_datasets}/**/*_nirs.snirf", recursive=True)
     for a in all_snirfs:
         s = a.split("_task-")[1]
         s = s.split("_nirs.snirf")[0]
@@ -198,11 +200,11 @@ def summarise_odpsd(raw, report):
 ########################################
 
 print(" ")
-Path(f"{args.bids_dir}/derivatives/fnirs-apps-quality-reports/").\
+Path(f"{args.output_location}/").\
     mkdir(parents=True, exist_ok=True)
 for id in ids:
     report = mne.Report(verbose=True, raw_psd=True)
-    report.parse_folder(f"{args.bids_dir}/sub-{id}", render_bem=False)
+    report.parse_folder(f"{args.input_datasets}/sub-{id}", render_bem=False)
     for idx, fname in enumerate(report.fnames):
         if mne.report._endswith(fname, 'nirs'):
             raw = mne.io.read_raw_snirf(fname)
@@ -215,7 +217,7 @@ for id in ids:
             raw, report = summarise_sci(raw, report, threshold=args.sci_threshold)
             raw, report = summarise_montage(raw, report)
 
-            report.save(f"{args.bids_dir}/derivatives/fnirs-apps-quality-reports/"
+            report.save(f"{args.output_location}/"
                         f"report_basic_{id}.html",
                         overwrite=True, open_browser=False)
 
