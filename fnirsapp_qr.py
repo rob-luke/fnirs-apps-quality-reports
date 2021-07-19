472e2e30c56b8088bbc3710b5a1a3677c627d94d
Robert Luke <hello@robertluke.net>
2021-07-19 17:13:53 +1000

    Use bids not snirf reader

--- fnirsapp_qr.py
+++ fnirsapp_qr.py
@@ -236,8 +236,7 @@ def run_report(path, path_out):
     report = mne.Report(verbose=True, raw_psd=True)
     report.parse_folder(f"{path.directory}", render_bem=False)
 
-    fname = report.fnames[0]
-    raw = mne.io.read_raw_snirf(fname)
+    raw = read_raw_bids(path)
     raw, report = plot_raw(raw, report)
     raw, report = summarise_triggers(raw, report)
     raw = optical_density(raw)
